5553a3ce66bebcc94eace2dade976a6045e257db
LOXXD <sijithappu7902@gmail.com>
2025-06-14 19:19:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,24 +3,24 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "24070854"))
+API_HASH = os.environ.get("API_HASH", "a1e7d63baa56a8bb6da56c1cb486fe4d")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5976019173:AAHEHuEYxWCiL5e95LHkqxOAA6Xc3_6ENOI")
+ADMIN = int(os.environ.get("ADMIN", "6686832098"))
 
 
 # Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQEYx1oAoS1kVr7tgMdfOJdPoFmo_oO5EY0hIQacIjR7iqQt9PPbO_VgAVzPQyKQk_MqSpizThm_S-qcxkme5sLgz-P0-TYJbAasZCpOKkDEicjAdu9BV91vbwIXMDxaMBeRtgteX5SydvXklSOPcHsxfIom778iqHMFEkXf_TxTxhzwMM0fOFM1TqA90n4FSx1HRGItSSzA251F8FDQTV1JBif9ZA7VRRfgtr3ID6AucOYpW8LaOdUZi4HGRrHOrszFfh0tZqglFPhKhI_P3yqYl1cO1boC4n4SmiaXtvX6rEeTn9EuG2sS5claSj4DFmTysUoZkhiItNQvUTzXWj5mmFHIGAAAAAAo0B41AA")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "cloudstore2024")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001829805583"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://renamebot:renamebot@cluster0.duek2xr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "renamebot")
 
 
 # Other Variables Config
